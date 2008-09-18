29f91168f1622da4fce81f6a9e9453c15c283cbb
brian d foy <brian@stonehenge.com>
2008-09-18 15:51:46 -0500

    * I think I've completely transferred everything into the App::Cpan module correctly, so that probably means that there are some bugs left.

---
+++ examples/MyCPANConfig.pm
@@ -0,0 +1,62 @@
+
+print "I'm being loaded!\n";
+
+$CPAN::Config = {
+                  'cpan_home' => '/Users/brian/Desktop/cpanhome',
+                  'makepl_arg' => '',
+                  'histfile' => '/Users/brian/Desktop/cpanhome/histfile',
+                  'unzip' => '/usr/bin/unzip',
+                  'show_upload_date' => '1',
+                  'dontload_hash' => {},
+                  'mbuild_install_build_command' => './Build',
+                  'lynx' => '',
+                  'curl' => '/usr/bin/curl',
+                  'ncftp' => '',
+                  'urllist' => [
+                                 'file:///MINICPAN',
+                                 'ftp://cpan.teleglobe.net/pub/CPAN'
+                               ],
+                  'gzip' => '/usr/bin/gzip',
+                  'ncftpget' => '',
+                  'keep_source_where' => '/Users/brian/Desktop/cpanhome/sources',
+                  'prefer_installer' => 'EUMM',
+                  'getcwd' => 'cwd',
+                  'make_install_make_command' => '/usr/bin/make',
+                  'no_proxy' => '',
+                  'build_cache' => '10',
+                  'make_arg' => '',
+                  'wget' => '',
+                  'auto_commit' => '1',
+                  'ftp_proxy' => '',
+                  'ftp_passive' => '1',
+                  'tar' => '/usr/bin/tar',
+                  'inactivity_timeout' => '0',
+                  'use_sqlite' => '0',
+                  'scan_cache' => 'atstart',
+                  'mbuildpl_arg' => '',
+                  'cache_metadata' => '1',
+                  'ftp' => '/usr/bin/ftp',
+                  'term_ornaments' => '1',
+                  'shell' => '/bin/bash',
+                  'prerequisites_policy' => 'follow',
+                  'make' => '/usr/bin/make',
+                  'gpg' => '',
+                  'mbuild_arg' => '',
+                  'inhibit_startup_message' => '0',
+                  'build_dir' => '/Users/brian/Desktop/cpanhome/build',
+                  'check_sigs' => '0',
+                  'commandnumber_in_prompt' => '1',
+                  'index_expire' => '1',
+                  'mbuild_install_arg' => '',
+                  'bzip2' => '/usr/bin/bzip2',
+                  'test_report' => '0',
+                  'pager' => '/usr/bin/less',
+                  'term_is_latin' => '1',
+                  'make_install_arg' => '',
+                  'colorize_output' => '0',
+                  'histsize' => '100',
+                  'http_proxy' => ''
+                };
+
+1;
+__END__
